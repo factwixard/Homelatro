acc2c6022ad9bb4216751df67f7f1140b0af9a37
factwixard <factwixard@gmail.com>
2025-08-22 22:34:02 -0700

    oops

--- src/enhancement.lua
+++ src/enhancement.lua
@@ -14,3 +14,11 @@ SMODS.Enhancement {
 	any_suit = true,
 	always_scores = true,
 }
+if next(SMODS.find_mod('artbox')) then
+local ref = Game.main_menu
+function Game:main_menu(change_context)
+ArtBox.add_collectible('m_hmlt_blank',
+	{ atlas = 'hmlt_hmltcollect', pos = { x = 0, y = 0 }, soul_pos = { x = 1, y = 0 } })
+	ref(self, change_context)
+end
+end
