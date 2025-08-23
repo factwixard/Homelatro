5b0171c6c29f3085fa9b6cdc791829949531e21b
factwixard <factwixard@gmail.com>
2025-08-22 22:21:32 -0700

    might work on removing more at some point

--- src/ownership.lua
+++ src/ownership.lua
@@ -618,56 +618,6 @@ SMODS.Consumable:take_ownership('hermit',
 	},
 	true
 )
-SMODS.Consumable:take_ownership('wheel_of_fortune',
-	{
-		config = { extra = { odds = 4 } },
-		loc_vars = function(self, info_queue, card)
-			return { vars = { G.GAME.probabilities.normal, card.ability.extra.odds } }
-		end,
-		use = function(self, card, area, copier)
-			if (pseudorandom('wheel_of_fortune') < G.GAME.probabilities.normal / card.ability.extra.odds and not next(SMODS.find_card("j_hmlt_hope"))) or (next(SMODS.find_card("j_hmlt_hope")) and pseudorandom('wheel_of_fortune') < (G.GAME.probabilities.normal * 2) / card.ability.extra.odds) then
-				local editionless_jokers = SMODS.Edition:get_edition_cards(G.jokers, true)
-				local eligible_card = pseudorandom_element(editionless_jokers, pseudoseed("wheel_of_fortune"))
-				local edition = poll_edition('wheel_of_fortune', nil, true, true,
-					{ 'e_polychrome', 'e_holo', 'e_foil' })
-				eligible_card:set_edition(edition, true)
-				check_for_unlock({ type = 'have_edition' })
-			else
-				G.E_MANAGER:add_event(Event({
-					trigger = 'after',
-					delay = 0.4,
-					func = function()
-						attention_text({
-							text = localize('k_nope_ex'),
-							scale = 1.3,
-							hold = 1.4,
-							major = card,
-							backdrop_colour = G.C.SECONDARY_SET.Tarot,
-							align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and
-								'tm' or 'cm',
-							offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and -0.2 or 0 },
-							silent = true
-						})
-						G.E_MANAGER:add_event(Event({
-							trigger = 'after',
-							delay = 0.06 * G.SETTINGS.GAMESPEED,
-							blockable = false,
-							blocking = false,
-							func = function()
-								play_sound('tarot2', 0.76, 0.4)
-								return true
-							end
-						}))
-						play_sound('tarot2', 1, 0.4)
-						card:juice_up(0.3, 0.5)
-						return true
-					end
-				}))
-			end
-		end
-	},
-	true
-)
 SMODS.Consumable:take_ownership('strength',
 	{
 		can_use = function(self, card)
